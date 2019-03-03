d07b08cc7e121e6fdd9dda01174ddf6ebe5c518b
Minegishi kai <kai.minegishi@gmail.com>
2019-03-03 17:24:50 +0900

    graphQLのコード埋め込み

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,47 @@ import './plugins/vuetify'
 import App from "./App.vue";
 import router from "./router";
 
+import { setContext } from 'apollo-link-context';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
+
 Vue.config.productionTip = false;
+
+Vue.use(VueApollo);
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql',
+  credentials: 'include'
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+// eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvZ3JhcGhxbCIsImlhdCI6MTU1MTU5MzYyNywiZXhwIjoxNTUxNTk3MjI3LCJuYmYiOjE1NTE1OTM2MjcsImp0aSI6ImV6UThaT1dDdk1mZXE0NVUiLCJzdWIiOjEsInBydiI6ImM4ZWUxZmM4OWU3NzVlYzRjNzM4NjY3ZTViZTE3YTU5MGI2ZDQwZmMifQ.YNMXFcRMfWQZvJvfWzAQFlrkm9kGmXmTgTJFz6a8diU
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -10,9 +10,6 @@
             label="E-mail"
             required
           ></v-text-field>
-        </v-flex>
-
-        <v-flex xs12 md4>
           <v-text-field
             v-model="pass"
             :rules="passRules"
@@ -20,11 +17,9 @@
             label="password"
             required
           ></v-text-field>
+          <v-btn color="primary" @click="login">login</v-btn>
         </v-flex>
 
-        <v-flex xs12 md4>
-          <v-btn　flat>login</v-btn>
-        </v-flex>
 
       </v-layout>
     </v-container>
@@ -32,21 +27,79 @@
 </template>
 
 <script>
+  import gql from 'graphql-tag';
+
+  const query = gql`
+    {
+      organization(login: "apollographql") {
+        repositories(first: 5) {
+          nodes {
+            id
+            name
+            url
+            viewerHasStarred
+            stargazers {
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const LOGIN = gql`
+    mutation($email: String!, $password: String!) {
+      Login(
+        email: $email
+        password: $password
+      ) {
+        access_token
+        token_type
+        expires_in
+      }
+    }
+  `;
+
+
   export default {
     data: () => ({
       valid: false,
-      id: '',
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
       pass: '',
       passRules: [
         v => !!v || 'id is required',
         v => v.length <= 10 || 'Name must be less than 10 characters'
       ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid'
-      ]
-    })
+      organization:  {}
+    }),
+    methods: {
+      login(e){       
+        let self = this; 
+        // ミューテーション
+        this.$apollo.mutate({
+          // Query
+          mutation: LOGIN,
+          // Parameters
+          variables: {
+            email: this.email,
+            password: this.pass
+          },
+        }).then((data) => {
+          console.log(data);
+        })
+
+      }
+    },
+    apollo: {
+      organization: {
+        query: query,
+        loadingKey: 'loading'
+      },
+    }
   }
 </script>
 
